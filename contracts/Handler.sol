e5bc8793a1f732b288b4ce925fe3b320ee3bf7c2
Kevin <kevin.k.2911@gmail.com>
2017-12-12 22:03:15 +0100

    added test function

--- contracts/Handler.sol
+++ contracts/Handler.sol
@@ -1,6 +1,9 @@
 pragma solidity ^0.4.14;
 
 contract Handler{
+    event test(
+        bytes4 a
+    );
     address owner;
     
     address requestHandler;
@@ -8,12 +11,15 @@ contract Handler{
     
     function Handler(address sender) public{
         owner = sender;
-        requestHandler =msg.sender;
+        requestHandler = msg.sender;
     }
     
-    function invoke(address toCall, string func) public{
-        require(msg.sender == requestHandler);
-        
-        bool ret = toCall.call(bytes4(keccak256(func)),5);
+    function invoke(address toCall, string func, string message, string proof) public{
+        //require(msg.sender == requestHandler);
+
+        bool ret = toCall.call(bytes4(keccak256(func)),5, message, proof);
+    }
+    function get(string func) constant returns(bytes4){
+        return bytes4(keccak256(func));
     }
 }
